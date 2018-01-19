4bb723d2270d8df437819980ad8762345830d728
Julien Noyer <dwsparis@MacBook-Pro-de-Julien.local>
2018-01-19 11:02:37 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,36 @@
+/*
+Importer les dépendances
+*/
+    // Composants
+    const express = require('express');
+    const path = require('path');
+    const ejs = require('ejs');
+
+    // Modules
+    const frontRoute = require('./routes/front');
+//
+
+/*
+Initialiser le serveur
+*/
+    // Configurer le serveur
+    const app = express();
+    const port = process.env.PORT || 3000;
+
+    // Configurer le dossier des vues client
+    app.set( 'views', __dirname + '/www' );
+    app.use( express.static(path.join(__dirname, 'www')) );
+
+    // Définir le moteur de rendu
+    app.engine( 'html', ejs.renderFile );
+    app.set( 'view engine', 'html' );
+
+    // Configurer les routes
+    app.use('/', frontRoute);
+//
+
+/*
+Lancer le serveur
+*/
+    app.listen( port, () => console.log( `Le serveur est lancé sur le port ${port}`) )
+//
